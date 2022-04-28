7a14b521a9ebf2893a9477e8fef28c4ffc96eccb
Oojin Ji <Oojin54@gmail.com>
2022-04-28 00:21:24 -0400

    Add project files.

---
+++ CodeFirstAssignment/DAL/EmployeeContext.cs
@@ -0,0 +1,27 @@
+﻿using CodeFirstAssignment.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+
+namespace CodeFirstAssignment.DAL
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext()
+            : base("EmpoyeeContext")
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ CodeFirstAssignment/Models/Department.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CodeFirstAssignment.Models
+{
+    public class Department
+    {
+        public int DepartmentID { get; set; }
+        public string Title { get; set; }
+        public Nullable<int> Credits { get; set; } 
+        
+        public virtual ICollection<Enrollment> Enrollments{ get; set; }
+    }
+}
